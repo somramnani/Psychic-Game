c0a7c316705b1f2c0e58534d278831e3def13c38
Som Ramnani <som.ramnani@gmail.com>
2019-03-09 21:47:09 -0500

    removed the refresh button

--- javascript/game.js
+++ javascript/game.js
@@ -1,8 +1,11 @@
+//__________________________________________________________
+//VARIABLES
+//__________________________________________________________
 
 // Create the alphabet
 var alphabet = [ "a" ,"b" , "c" , "d" , "e"  , "f"  , "g"  , "h", "i" ,
                   "j"  , "k"  , "l"  , "m"  ,"n" , "o" , "p" , "q", "r" ,"s", 
-                  "t", "u" , "v", "w" , "x", "y", , "z"];
+                  "t", "u" , "v", "w" , "x", "y",  "z"];
 
 // Creating variables to hold the number of wins, losses, guessCount, and the array for the users guesses. 
 var wins = 0;
@@ -13,14 +16,17 @@ var currentGuesses  = [];
 // Create variables that hold references to the places in the HTML where we want to display things.
 var directionsText = document.getElementById("directions-text");
 var userChoiceText = document.getElementById("userchoice-text");
-
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessText =  document.getElementById("guess-text");
 
- // Create the computers random letter
+// Create the computers random letter
 var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 
+
+//__________________________________________________________
+//GAME FUNCTION
+//__________________________________________________________
 document.onkeyup = function(event){
     
     // Grabbing the users guesses when they press on the keyboard
@@ -39,7 +45,8 @@ document.onkeyup = function(event){
         // Resets the computer guesss
         computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
         
-    }// if the user is out of guesses:
+    }
+    // if the user is out of guesses:
     else if (guessCount === 0){
         alert("You lost! The letter I was thinking of was " + computerGuess);
         
@@ -67,5 +74,4 @@ document.onkeyup = function(event){
     lossesText.textContent = "Losses: " + losses;
     guessText.textContent = "Guesses:" + guessCount;
 }
-
 
